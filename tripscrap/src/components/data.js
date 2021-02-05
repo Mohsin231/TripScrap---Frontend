c021c25f25de117cda1de4af37de458395509126
michellecgude <michellecgude@gmail.com>
2021-02-05 09:41:50 -0600

    fixing merge conflicts.

--- tripscrap/src/components/data.js
+++ tripscrap/src/components/data.js
@@ -1,20 +1,34 @@
-import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect} from 'react';
 
+<<<<<<< HEAD
 function Data(props) {
+=======
+// function data(props) {
+>>>>>>> 9a8e11a (downloaded bootstrap, tested out possible options, etc.)
 
-    const [data, setData] = useState([])
+//     const [data, setData] = useState([])
 
+<<<<<<< HEAD
       
   return (
 
       <>
     <h1>data</h1>
+=======
+//   return (
+//       <>
+//     <h1>data</h1>
+>>>>>>> 9a8e11a (downloaded bootstrap, tested out possible options, etc.)
 
-    <button>Save</button>
+//     <button>Save</button>
 
-    </>
-  );
+//     </>
+//   );
 
-}
+// }
 
+<<<<<<< HEAD
 export default Data;
+=======
+// export default data;
+>>>>>>> 9a8e11a (downloaded bootstrap, tested out possible options, etc.)
--- tripscrap/src/components/tripScrap.js
+++ tripscrap/src/components/tripScrap.js
@@ -1,21 +1,41 @@
-import React, {useState, Component} from 'react';
-import Data from '../components/data'
+// import React, {useState, Component} from 'react';
+// import Data from '../components/data'
 
+<<<<<<< HEAD
 
 function TripScrap() {
+=======
+// class tripScrap extends Component{
+//     render(){
+//         return <div></div>
+//     }
+// }
 
-    const [data,setData] = useState([]);
+// function tripScrap() {
+>>>>>>> 9a8e11a (downloaded bootstrap, tested out possible options, etc.)
 
-    const onClick = (e) => {
-        e.preventDefault();
-        console.log("button clicked")
-    }
+//     const [data,setData] = useState([]);
 
+//     const onClick = (e) => {
+//         e.preventDefault();
+//         console.log("button clicked")
+//     }
+
+<<<<<<< HEAD
   return (
     <h1>data</h1>
 
   );
+=======
+//   return (
+//     <h1>data</h1>
+//   );
+>>>>>>> 9a8e11a (downloaded bootstrap, tested out possible options, etc.)
   
-}
+// }
 
+<<<<<<< HEAD
 export default TripScrap;
+=======
+// export default tripScrap;
+>>>>>>> 9a8e11a (downloaded bootstrap, tested out possible options, etc.)
