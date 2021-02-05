85537726db99df5b3970efbf840f6b979e76d759
michellecgude <michellecgude@gmail.com>
2021-02-05 14:33:33 -0600

    built input/button through bootstrap and todo/tripscrap list.

--- tripscrap/src/App.js
+++ tripscrap/src/App.js
@@ -1,23 +1,87 @@
-import { useState } from "react";
 import "./App.css";
-import TripScrap from "./components/tripScrap";
-import destination from "./components/destination";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table'
+
+
+// import { useState } from "react";
+// import TripScrap from "./components/tripScrap";
+// import Destination from "./components/destination";
+
+// this was below:
+// {/* <TripScrap /> */ }
+// {/* <Destination /> */}
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>TripScrap</h1>
-      </header>
-
-      <div class="grid-container">
-        <div class="side_bar"></div>
-        <div class="main_card">
-          <TripScrap />
-        </div>
+      <div class="side_bar">
+        <Container>
+          <Row>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="What do you want your tripscrap name to be?"
+                aria-label="Tripscrap name"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="dark">Button</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Row>
+          <Row>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Where do you want to go?"
+                aria-label="Tripscrap todo"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="dark">Button</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Row>
+        </Container>
+        <Container>
+          <Table striped bordered hover variant="light">
+            <thead>
+              <tr>
+                <th>to do</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>tripscrap todo placeholder!</td>
+              </tr>
+              <tr>
+                <td>tripscrap todo placeholder!</td>
+              </tr>
+              <tr>
+                <td>tripscrap todo placeholder!</td>
+              </tr>
+              <tr>
+                <td>tripscrap todo placeholder!</td>
+              </tr>
+              <tr>
+                <td>tripscrap todo placeholder!</td>
+              </tr>
+              <tr>
+                <td>tripscrap todo placeholder!</td>
+              </tr>
+              <tr>
+                <td>tripscrap todo placeholder!</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Container>
       </div>
-
-      <destination />
+      <div class="main_card"></div>
     </div>
   );
 }
--- tripscrap/src/index.js
+++ tripscrap/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-<App />, document.getElementById('root'));
-
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
