4dbb7eb0ed3f685e9ea6e3d40ed23e15d5601e03
ruotolbr1 <ruotolbr@gmail.com>
2021-02-08 12:51:10 -0500

    cleaned up form and added some input functionality with onClick

--- tripscrap/src/components/tripScrap.js
+++ tripscrap/src/components/tripScrap.js
@@ -1,20 +1,42 @@
-import React, { useState, Component } from "react";
-import Data from "../components/Data";
+// import React, { useState, Component } from "react";
+// import Data from "../components/Data";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Button from "react-bootstrap/Button";
 
-function TripScrap() {
-  const initialState = {
-    tripScrapName: '', 
-    whereAreYouGoing: '', 
-    todoList: '', 
-};
-  const [data, setData] = useState(initialState);
+// function TripScrap() {
+//   const initialState = {
+//     tripScrapName: '', 
+//     whereAreYouGoing: '', 
+//     todoList: '', 
+// };
+// //   const [data, setData] = useState(initialState);
+// //  const handleSubmit = (event) => {
+// //    // we always need to stop the browser
+// //   // from submitting the form or the page
+// //   // will be refreshed.
+// //   event.preventDefault();
+// //   // do something with the data in the component state
+// //   console.log(data);
+// //   // clear the form
+// //   setFormState(initialState);
+// // };
 
-  const onClick = (e) => {
-    e.preventDefault();
-    console.log("button clicked");
-  };
+// // // Event Listener: tells the browser
+// // // which event to listen for on which
+// // // element and what to do when the event
+// // // happens
+// // <form onSubmit={handleSubmit}></form>
+// //  }
+// //   // const onClick = (e) => {
+// //   //   e.preventDefault();
+// //   //   console.log("button clicked");
+// //   // };
 
-  return <h1>data</h1>;
-}
+//   return 
 
-export default TripScrap;
+
+
+
+// }
+
+// export default TripScrap;
